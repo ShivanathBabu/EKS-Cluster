0b6eda525b0e9467782a545d5e82dc96e047fb84
shivanathbabu kodimyala <shivanathbabu.k@websynergies.com>
2026-05-18 17:14:47 +0530

    sg

---
+++ EKS-cluster-setup/sg/parameter.tf
@@ -0,0 +1,18 @@
+resource "aws_ssm_parameter" "bastion" {
+  name = "/${var.project}/${var.environment}/bastion"
+  type = "String"
+  value = module.bastion.sg_id
+}
+
+resource "aws_ssm_parameter" "ekscluster" {
+  name = "/${var.project}/${var.environment}/ekscluster"
+  type = "String"
+  value = module.cluster.sg_id
+}
+
+resource "aws_ssm_parameter" "eksnode" {
+  name = "/${var.project}/${var.environment}/eksnode"
+  type = "String"
+  value = module.node.sg_id
+}
+
