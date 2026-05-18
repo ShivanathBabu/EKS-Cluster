ad2339319636f5dd7f1cc4f2f1e447c97779404d
shivanathbabu kodimyala <shivanathbabu.k@websynergies.com>
2026-05-18 11:39:30 +0530

    eks-cluster

--- EKS-cluster-setup/vpc/parameter.tf
+++ EKS-cluster-setup/vpc/parameter.tf
@@ -7,11 +7,11 @@ resource "aws_ssm_parameter" "vpc_id" {
 resource "aws_ssm_parameter" "public" {
   name = "/${var.project}/${var.environment}/public_subnet"
   type = "StringList"
-  value = join(",",module.public_subnet)
+  value = join(",",module.vpc.public_subnet)
 }
 
 resource "aws_ssm_parameter" "private" {
   name = "/${var.project}/${var.environment}/private_subnet"
   type = "StringList"
-  value = join(",",module.private_subnet)
+  value = join(",",module.vpc.private_subnet)
 }
