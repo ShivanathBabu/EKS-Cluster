c107e7f9ef3bea5b416f3fa0e2df7c4cb0c3df0b
shivanathbabu kodimyala <shivanathbabu.k@websynergies.com>
2026-05-23 16:43:26 +0530

    eks

--- VPC-module/peering.tf
+++ VPC-module/peering.tf
@@ -32,4 +32,5 @@ resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1:0
   route_table_id = data.aws_route_table.default.id
   destination_cidr_block = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.foo[count.index].id 
 }
